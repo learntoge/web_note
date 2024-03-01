70e8b7342eed47680f2bad8b065dfd1582c1fa71
xuexi_together <2028568785@qq.com>
2024-03-01 21:31:58 +0800

    Learn_together_release
    今天发布了Learn_Together网站第一个版本

---
+++ js/indexxuanran.js
@@ -0,0 +1,72 @@
+document.querySelector('body').innerHTML=`  
+<!-- 导航栏模块 -->
+<nav>
+    <div class="nav_big_box">
+        <span class="logotext"><a href="index.html"><img src="img/logo.png" alt=""><p>最美明天</p></a></span>
+        <span class="nav_text">
+            <span><a href="index.html">首页</a></span>
+            <span class="menus"><a href="">学习笔记</a>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </span>
+            <span class="menus"><a href="">资料</a>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul></span>
+            <span class="menus"><a href="">备用1</a>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul></span>
+            <span class="menus"><a href="">备用2</a>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul></span>
+            <span class="menus"><a href="">备用3</a>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul></span>
+        </span>
+        <!-- <span class="shengming">说明</span> -->
+    </div>
+    <div style="height: 300px; width: 1300px;margin: 30px auto 0;
+    background-color: #ffffff67;"></div>
+</nav>
+
+
+
+
+
+
+
+
+
+
+
+<!-- 页脚模块 -->
+<footer>
+
+</footer>
+
+
+`
