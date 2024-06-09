960bbc868f3da9ca27ad6c325dd0aebafea6f6b9
xuexi_together <89248771+learntoge@users.noreply.github.com>
2024-06-09 15:49:20 +0800

    Update gkdaojishu.js

--- js/gkdaojishu.js
+++ js/gkdaojishu.js
@@ -6,16 +6,38 @@ setInterval(function () {
     const text_fen = document.querySelector('.fen');
     const text_shi = document.querySelector('.shi')
     // 定义时间
-    let last_time = new Date('2024-06-07 00:00:00')
+    let last_time = new Date('2024-06-07 09:00:00')
+    let end_time = new Date('2024-06-09 18:15:00')
+
     let now_time = new Date();
     let times = (last_time - now_time) / 1000
-    let d = parseInt(times / 60 / 60 / 24);
-    text_tian.innerHTML = (d < 10 ? "0" + d : d) + '天';
-    let h = parseInt(times / 60 / 60 % 24);
-    text_shi.innerHTML = (h < 10 ? "0" + h : h) + '时';
-    let m = parseInt(times / 60 % 60);
-    text_fen.innerHTML = (m < 10 ? "0" + m : m) + '分';
-    let s = parseInt(times % 60);
-    text_miao.innerHTML = (s < 10 ? "0" + s : s) + '秒';
+    if (times >= 0) {
+        let d = parseInt(times / 60 / 60 / 24);
+        text_tian.innerHTML = (d < 10 ? "0" + d : d) + '天';
+        let h = parseInt(times / 60 / 60 % 24);
+        text_shi.innerHTML = (h < 10 ? "0" + h : h) + '时';
+        let m = parseInt(times / 60 % 60);
+        text_fen.innerHTML = (m < 10 ? "0" + m : m) + '分';
+        let s = parseInt(times % 60);
+        text_miao.innerHTML = (s < 10 ? "0" + s : s) + '秒';
+    }
+    else {
+        if (now_time <= end_time) {
+            text_tian.innerHTML = '高';
+            text_shi.innerHTML = '考';
+            text_fen.innerHTML = '开';
+            text_miao.innerHTML = '始';
+        }
+        else {
+            text_tian.innerHTML = '高';
+            text_shi.innerHTML = '考';
+            text_fen.innerHTML = '结';
+            text_miao.innerHTML = '束';
+        }
+
+    }
+
+
+
 
 }, 1000)
